3cc5d3ab20c884bf80985e55c4fb6d623e65daec
vedansh1101 <vedanshpanchariya14@gmail.com>
2026-01-02 19:30:20 +0530

    Initial project upload

--- server/index.js
+++ server/index.js
@@ -15,7 +15,8 @@ console.log(
   process.env.GEMINI_API_KEY ? "Yes ✅" : "No ❌"
 );
 
-const genAI = new GoogleGenerativeAI(import.meta.env.GEMINI_API_KEY);
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+
 
 
 // ✅ Health check
